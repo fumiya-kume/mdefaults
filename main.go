98c60baf67df8c64e659542744f46068b41bbd5a
fumiya-kume <fumiya.kume@hotmail.com>
2025-01-05 18:00:30 +0900

    Write unit test for file system access

--- main.go
+++ main.go
@@ -7,18 +7,31 @@ import (
 	"path/filepath"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+// FileSystem interface for file operations
+type FileSystem interface {
+	UserHomeDir() (string, error)
+	Stat(name string) (os.FileInfo, error)
+	Create(name string) (*os.File, error)
+}
 
-	home, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Println(err)
-	}
-	configFile := filepath.Join(home, ".config", ".mdefaults")
-	if _, err := os.Stat(configFile); os.IsNotExist(err) {
-		fmt.Println("Config file not found, creating it")
-		os.Create(configFile)
-	}
+// OSFileSystem is a concrete implementation of FileSystem using the os package
+type OSFileSystem struct{}
+
+func (OSFileSystem) UserHomeDir() (string, error) {
+	return os.UserHomeDir()
+}
+
+func (OSFileSystem) Stat(name string) (os.FileInfo, error) {
+	return os.Stat(name)
+}
+
+func (OSFileSystem) Create(name string) (*os.File, error) {
+	return os.Create(name)
+}
+
+func main() {
+	fs := OSFileSystem{}
+	setupConfigFile(fs)
 
 	defaults := &DefaultsCommandImpl{
 		domain: "com.apple.dock",
@@ -30,4 +43,17 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(result)
+}
+
+func setupConfigFile(fs FileSystem) {
+	home, err := fs.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	configFile := filepath.Join(home, ".config", ".mdefaults")
+	if _, err := fs.Stat(configFile); os.IsNotExist(err) {
+		fmt.Println("Config file not found, creating it")
+		fs.Create(configFile)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+// MockFileSystem is a mock implementation of the FileSystem interface
+type MockFileSystem struct {
+	homeDir   string
+	statError error
+	createErr error
+}
+
+func (m MockFileSystem) UserHomeDir() (string, error) {
+	return m.homeDir, nil
+}
+
+func (m MockFileSystem) Stat(name string) (os.FileInfo, error) {
+	return nil, m.statError
+}
+
+func (m MockFileSystem) Create(name string) (*os.File, error) {
+	return nil, m.createErr
+}
+
+func TestSetupConfigFile_CreatesFileIfNotExist(t *testing.T) {
+	fs := MockFileSystem{
+		homeDir:   "/mock/home",
+		statError: os.ErrNotExist,
+	}
+
+	setupConfigFile(fs)
+	if fs.createErr != nil {
+		t.Errorf("Expected create error to be nil but got %v", fs.createErr)
+	}
+}
+
+func TestSetupConfigFile_DoesNotCreateFileIfExists(t *testing.T) {
+	fs := MockFileSystem{
+		homeDir:   "/mock/home",
+		statError: nil,
+	}
+
+	setupConfigFile(fs)
+	if fs.createErr != nil {
+		t.Errorf("Expected create error to be nil but got %v", fs.createErr)
+	}
+}
+
+func TestSetupConfigFile_HandleUserHomeDirError(t *testing.T) {
+	fs := MockFileSystem{
+		homeDir:   "",
+		statError: nil,
+	}
+
+	setupConfigFile(fs)
+	if fs.createErr != nil {
+		t.Errorf("Expected create error to be nil but got %v", fs.createErr)
+	}
+}
