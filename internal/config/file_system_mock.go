915e4d3e3b46536a0323c80ead387308f080f839
fumiya-kume <fumiya.kume@hotmail.com>
2025-03-21 12:53:01 +0900

    Add e2e test config

--- internal/config/file_system_mock.go
+++ internal/config/file_system_mock.go
@@ -10,6 +10,7 @@ type MockFileSystem struct {
 	WriteFileContent  string
 }
 
+// ReadFile implements the FileSystemReader interface for MockFileSystem.
 func (m *MockFileSystem) ReadFile(name string) (string, error) {
 	if m.StatError != nil {
 		return "", m.StatError
@@ -17,6 +18,7 @@ func (m *MockFileSystem) ReadFile(name string) (string, error) {
 	return m.ConfigFileContent, nil
 }
 
+// WriteFile implements the FileSystemReader interface for MockFileSystem.
 func (m *MockFileSystem) WriteFile(name string, content string) error {
 	m.WriteFileContent = content
 	return m.WriteFileErr
